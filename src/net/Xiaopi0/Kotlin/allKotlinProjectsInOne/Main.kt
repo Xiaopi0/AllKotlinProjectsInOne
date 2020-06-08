8cc2981fe81f658f8b7f932210d6f29bf41175bb
Xiaopi0 <63703930+Xiaopi0@users.noreply.github.com>
2020-06-08 09:52:55 +0200

    Initial commit

--- src/net/Xiaopi0/Kotlin/allKotlinProjectsInOne/Main.kt
+++ src/net/Xiaopi0/Kotlin/allKotlinProjectsInOne/Main.kt
@@ -4,4 +4,20 @@ import java.util.*
 
 fun main() {
     val scanner = Scanner(System.`in`)
+
+    println("Welcome to all Kotlin projects made by Xiaopi0.")
+    println("Here is a list of them:")
+    println("1. test")
+    println("To run a project type the number the project has on the list(without the dot).")
+    val in1 = scanner.nextInt()
+
+    if (in1 == 1) {
+        Test()
+    }
+}
+
+fun Test() {
+    println("Hi, I'm a test.")
+    println()
+    main()
 }
