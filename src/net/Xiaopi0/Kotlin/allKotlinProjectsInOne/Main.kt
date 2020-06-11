5797b1c2e6245dbd5c68d856ee6921e4d319484f
Xiaopi0 <63703930+Xiaopi0@users.noreply.github.com>
2020-06-11 14:11:22 +0200

    Initial commit

--- src/net/Xiaopi0/Kotlin/allKotlinProjectsInOne/Main.kt
+++ src/net/Xiaopi0/Kotlin/allKotlinProjectsInOne/Main.kt
@@ -1,6 +1,7 @@
 package net.Xiaopi0.Kotlin.allKotlinProjectsInOne
 
 import java.util.*
+import net.Xiaopi0.Kotlin.allKotlinProjectsInOne.*
 
 fun main() {
     val scanner = Scanner(System.`in`)
@@ -14,52 +15,6 @@ fun main() {
     if (in1 == -1) {
         println("Quitting")
     }else if (in1 == 1) {
-        calculator1()
+        calculator()
     }
-}
-
-fun calculator1() {
-    val scanner = Scanner(System.`in`)
-
-    var loop = true
-    while (loop) {
-        println("Write two numbers to do things with and which operator you want to use. \n? to see list of commands")
-        val input_1 = scanner.next()
-        //Commands
-        if (input_1 == "?") {
-            println("List of commands:")
-            println()
-            println("1. ? = view this list.")
-            println("2. quit = quits the application.")
-            println()
-        } else if (input_1 == "quit") {
-            loop = false
-        } else {
-            val input_2 = scanner.next()
-            val output_1 = input_1.toInt()
-            val output_2 = input_2.toInt()
-            val operator_1 = scanner.next()
-            if (operator_1 == "+") {
-                println(output_1 + output_2)
-                println()
-            }else if (operator_1 == "-") {
-                println(output_1 - output_2)
-                println()
-            }else if (operator_1 == "*" || operator_1 == "x") {
-                println(output_1 * output_2)
-                println()
-            }else if (operator_1 == "/") {
-                println(output_1 / output_2)
-                println()
-            }else if (operator_1 == "%") {
-                println(output_1 % output_2)
-                println()
-            }else {
-                println("Error: invalid operator.")
-            }
-        }
-
-    }
-    println()
-    main()
 }
---
+++ src/net/Xiaopi0/Kotlin/allKotlinProjectsInOne/calculator.kt
@@ -0,0 +1,49 @@
+package net.Xiaopi0.Kotlin.allKotlinProjectsInOne
+
+import java.util.*
+
+fun calculator() {
+    val scanner = Scanner(System.`in`)
+
+    var loop = true
+    while (loop) {
+        println("Write two numbers to do things with and which operator you want to use. \n? to see list of commands")
+        val input_1 = scanner.next()
+        //Commands
+        if (input_1 == "?") {
+            println("List of commands:")
+            println()
+            println("1. ? = view this list.")
+            println("2. quit = quits the application.")
+            println()
+        } else if (input_1 == "quit") {
+            loop = false
+        } else {
+            val input_2 = scanner.next()
+            val output_1 = input_1.toInt()
+            val output_2 = input_2.toInt()
+            val operator_1 = scanner.next()
+            if (operator_1 == "+") {
+                println(output_1 + output_2)
+                println()
+            } else if (operator_1 == "-") {
+                println(output_1 - output_2)
+                println()
+            } else if (operator_1 == "*" || operator_1 == "x") {
+                println(output_1 * output_2)
+                println()
+            } else if (operator_1 == "/") {
+                println(output_1 / output_2)
+                println()
+            } else if (operator_1 == "%") {
+                println(output_1 % output_2)
+                println()
+            } else {
+                println("Error: invalid operator.")
+            }
+        }
+
+    }
+    println()
+    main()
+}
